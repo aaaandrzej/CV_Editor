1f8a1f4dbec32c639d55a311c7f3dd64971f81fd
andrzej <andrzej.szulc@gmail.com>
2020-04-04 16:31:30 +0200

    formularz html do dodawania cv metoda POST w /cv + poprawki i porzadki w kodzie

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ def index():
     return "For API please use /api/cv or /api/cv/<id>, for WWW please use /cv or /cv<id>"
 
 
-@app.route('/api/cv', methods=['GET', 'POST']) # API
+# API
+
+@app.route('/api/cv', methods=['GET', 'POST'])
 def api_cv():
     if request.method == "GET":
         return query_read_db("select_all.sql")
@@ -25,65 +27,66 @@ def api_cv():
         return f"{query_data} cv added to db"
 
 
-@app.route('/cv', methods=['GET', 'POST']) # WWW
-def cv():
+@app.route('/api/cv/<id>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def api_cv_id(id=None):
     if request.method == "GET":
-        all_db_records = query_read_db("select_all.sql")
-        all_db_records_json = json.loads(all_db_records)
-        return render_template("index.html", all_db_records=all_db_records_json)
+        return query_read_one_from_db("select_one.sql", id)
+
+    elif request.method == "POST":  # TODO co tu robić? nadpisywać dane cv? to ma sens?
+        return f"post cv {id} not yet supported"
+
+    elif request.method == "DELETE":
+        query_remove_from_db("delete_one.sql", id)
+        return f"cv {id} successfully removed"
+
+    elif request.method == "PUT":  # TODO co tu robić? modyfikacja danego cv?
+        return f"put cv {id} not yet supported"
+
+    else:
+        return f'{request.method} cv {id} not yet supported'
 
 
-'''
-# przerobić, dodać formularz - póki co post to kopia z api
-    elif request.method == "POST":  # przerobić to wszystko na request.form.get..
-        query_data = request.get_json()
+# HTML
+
+@app.route('/cv', methods=['GET', 'POST'])
+def cv():
+
+    all_db_records = query_read_db("select_all.sql")
+    all_db_records_json = json.loads(all_db_records)
+
+    if request.method == "GET":
+        pass
+
+    if request.method == "POST":
+        query_data = {
+                  'firstname': request.form.get('firstname_from_form'),
+                  'lastname': request.form.get('lastname_from_form'),
+                  'python': request.form.get('python_from_form'),
+                  'javascript': request.form.get('javascript_from_form'),
+                  'sql': request.form.get('sql_from_form'),
+                  'english': request.form.get('english_from_form')
+                  }
 
         query_insert_db("insert_cv.sql", query_data)
-        return f"{query_data} cv added to db"
-'''
+
+        # TODO jak wymusić przeładowanie tej strony po dodaniu nowego CV???
+
+    return render_template("index.html", all_db_records=all_db_records_json)
 
 
-@app.route('/api/cv/<id>', methods=['get', 'post', 'put', 'delete']) # API
-def api_cv_id(id=None):
-    if request.method == "GET":  # DONE
-        return query_read_one_from_db("select_one.sql", id)
+@app.route('/cv/<id>', methods=['GET', 'POST'])
+def cv_id(id=None):
+    if id is None:
+        return "czyżbyś wszedł na /cv/ ?"  # TODO czemu to nie działa...?
 
-    elif request.method == "POST":
+    if request.method == "GET":  # TODO todosek w htmlku jak wyłączyć/ zmienić formularz
+        one_db_record = query_read_one_from_db("select_one.sql", id)
+        one_db_record_json = json.loads(one_db_record)
+        return render_template("index.html", all_db_records=one_db_record_json)
+
+    elif request.method == "POST":  # TODO co z tym robimy - tzn co to powinno robić?
         return f"post cv {id} not yet supported"
-
-    elif request.method == "DELETE":  # DONE
-        query_remove_from_db("delete_one.sql", id)
-        return f"cv {id} successfully removed"
-
-    elif request.method == "PUT":
-        return f"put cv {id} not yet supported"
-
-    else:
-        return f'{request.method} cv {id} not yet supported'
-
-'''
-#przerobić analogicznie jak /cv
-@app.route('/cv/<id>', methods=['get', 'post', 'put', 'delete']) # WWW
-def api_cv_id(id=None):
-    if request.method == "GET":  # DONE
-        return query_read_one_from_db("select_one.sql", id)
-
-    elif request.method == "POST":  # to samo co 21 - przekazywać form get czy json?
-        return f"post cv {id} not yet supported"
-
-    elif request.method == "DELETE":  # DONE
-        query_remove_from_db("delete_one.sql", id)
-        return f"cv {id} successfully removed"
-
-    elif request.method == "PUT":  # to samo co 21
-        return f"put cv {id} not yet supported"
-
-    else:
-        return f'{request.method} cv {id} not yet supported'
-'''
 
 
 if __name__ == '__main__':
-    # cvs = query_read_db(select_all)
-    # print(str(cvs))
     app.run(debug=True)
