2fd12b8d101fc99329dc3dc4d710a804b9b2fe1c
andrzej <andrzej.szulc@gmail.com>
2020-08-10 14:50:06 +0200

    assoc object, kolejna proba, do przerobienia dzialajacy model z tutka sqlalchemy. stary model w models_almost_there.py - docelowo do poprawki

--- app/_temp.py
+++ app/_temp.py
@@ -1,41 +1,69 @@
-from models import User, Skill, UserSkill
-# from models import Skill, User, SkillsToUsers
+# from models import User, Skill, SkillUser
+from models import Child, Parent, Association
 from session import get_session
 
-if __name__ == '__main__':
+session = get_session(echo=False)
 
-    session = get_session(echo=False)
-# """
-user1 = User('karol')
-user1.skills = {
-    'js':1,
-    'sql':5
-    }
 
-user2 = User('basia')
-# user2.skills = {
-#     'english':5,
-#     }
+# create parent, append a child via association
+p = Parent()
+a = Association(extra_data="some data")
+a.child = Child()
+p.children.append(a)
 
-print(user1.name, user1.skills)
-print(user2.name, user2.skills)
+# iterate through child objects via association, including association
+# attributes
+for assoc in p.children:
+    print(assoc.extra_data)
+    print(assoc.child)
 
-session.add(user1)
-session.add(user2)
-session.commit()
 
-# """
+
+
+
+
+
+
+"""
+# user1 = User()
+user2 = User()
+
+user2.username = "andrzej"
+
+sql = Skill()
+sql.skill_name = "sql"
+sql.skill_level = 0
+
+python = Skill()
+python.skill_name = "python"
+# python.skill_level = 2
+
+user2.skills.append(sql)
+user2.skills.append(python)
 #
+print(user2.username, user2.skills)
+#
+# for skl in user2.skills:
+#     print(skl.skill_name, skl.skill_level)
 
-# user = session.query(User).get(2)
-# print(user.skills)
 
+# print(user1.name, user1.skills)
+# print(user2.name, user2.skills)
+#
 # session.add(user1)
-# session.add(user2)
+session.add(user2)
 # session.commit()
 
 
-# w konsoli:
-# alembic downgrade base
-# alembic revision --autogenerate -m "start"
-# alembic upgrade head
+#
+
+# user = session.query(User).get(1)
+# print(user.name, user.skills)
+#
+# session.add(user1)
+# session.add(user2)
+# session.commit()
+"""
+
+
+
