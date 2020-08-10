5c43147ca64811747d51c801f98c4ed98cb92d7b
andrzej <andrzej.szulc@gmail.com>
2020-08-10 16:24:24 +0200

    dzialajacy model z assoc object

--- app/_temp.py
+++ app/_temp.py
@@ -1,69 +1,84 @@
-from models import User, Skill, SkillUser
+from models import User, SkillName, SkillUser
 # from models import Child, Parent, Association
 from session import get_session
 
 session = get_session(echo=False)
 
-
-# create parent, append a child via association
-u = User()
-su = SkillUser(extra_data="some data")
-su.skill = Skill()
-u.skills.append(su)
-
-# iterate through child objects via association, including association
-# attributes
-for sku in u.skills:
-    print(sku.extra_data)
-    print(sku.skill)
-
-session.add(u)
-session.commit()
-
-
-
-
-
 """
-# user1 = User()
-user2 = User()
+andrzej = User()
+sql = SkillUser(skill_level=1)
+sql.skill = SkillName()
+andrzej.skills.append(sql)
 
-user2.username = "andrzej"
 
-sql = Skill()
-sql.skill_name = "sql"
-sql.skill_level = 0
+for sku in andrzej.skills:
+    print(sku.skill_level)
+    print(sku.skill_id)
 
-python = Skill()
-python.skill_name = "python"
-# python.skill_level = 2
+session.add(andrzej)
+# session.commit()
 
-user2.skills.append(sql)
-user2.skills.append(python)
+print(andrzej.skills)
+"""
+
+# user = session.query(User).get(1)
 #
-print(user2.username, user2.skills)
+andrzej = User()
+piotr = User()
+
+andrzej.username = "andrzej"
+piotr.username = "piotr"
+
+
+python_name = SkillName()
+python_name.skill_name = "python"
+
+python_piotra = SkillUser()
+python_piotra.skill = python_name
+python_piotra.skill_level = 5
+
+
+python2_name = SkillName()
+python2_name.skill_name = "python"
+
+
+sql_name = SkillName()
+sql_name.skill_name = "sql"
+
+sql_piotra = SkillUser()
+sql_piotra.skill = sql_name
+sql_piotra.skill_level = 4
+
+
+# user2.skills.append(sql)
+piotr.skills.append(python_piotra)
+piotr.skills.append(sql_piotra)
 #
-# for skl in user2.skills:
-#     print(skl.skill_name, skl.skill_level)
+# print(user.username, user.skills)
+#
+for skl in piotr.skills:
+    print(skl.user.username, end=": ")
+    print(skl.skill.skill_name, skl.skill_level)
+    # print(type(skl))
+    # print()
 
-
-# print(user1.name, user1.skills)
-# print(user2.name, user2.skills)
+print()
+print(andrzej.username, andrzej.skills)
+print(piotr.username, piotr.skills)
 #
 # session.add(user1)
-session.add(user2)
+# session.add(user)
 # session.commit()
 
 
 #
 
-# user = session.query(User).get(1)
 # print(user.name, user.skills)
 #
 # session.add(user1)
 # session.add(user2)
 # session.commit()
-"""
+# """
 
 
 
--- app/models.py
+++ app/models.py
@@ -7,25 +7,40 @@ Base = declarative_base()
 
 class SkillUser(Base):
     __tablename__ = 'skill_user'
+
     user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
     skill_id = Column(Integer, ForeignKey('skill.id'), primary_key=True)
-    extra_data = Column(String(50))
-    # child = relationship("Child", back_populates="parents")
-    skill = relationship("Skill", back_populates="users")
-    # parent = relationship("Parent", back_populates="children")
+    skill_level = Column(Integer())
+
+    skill = relationship("SkillName", back_populates="users")  # czy skills? czy user?
     user = relationship("User", back_populates="skills")
+
+    # skill_name = relationship("SkillName", back_populates="skill_name")
+
+    def __repr__(self):
+        return f"{self.skill.skill_name} {self.skill_level}"
 
 
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
+    username = Column(String)  # nullable=False)
+    password = Column(String)  # nullable=False)
+    firstname = Column(String)  # nullable=False)
+    lastname = Column(String)  # nullable=False)
 
     skills = relationship("SkillUser", back_populates="user")
 
+    def __repr__(self):
+        return f"{self.firstname} {self.lastname}"
 
-class Skill(Base):
+
+class SkillName(Base):
     __tablename__ = 'skill'
     id = Column(Integer, primary_key=True)
+    skill_name = Column(String(), nullable=False)  # , unique=True)
 
-    # parents = relationship("Association", back_populates="child")
-    users = relationship("SkillUser", back_populates="skill")
+    users = relationship("SkillUser", back_populates="skill")  # czy skills? czy user?
+
+    def __repr__(self):
+        return f"{self.skill_name}"
