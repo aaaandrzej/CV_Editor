b9921ad3b49d4687419febe824baa298e6bc97ad
andrzej <andrzej.szulc@gmail.com>
2020-08-24 13:05:04 +0200

    poprawka w session.py

--- app/session.py
+++ app/session.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/my_db')
+session_factory = sessionmaker(bind=engine)
 
-def get_session(echo: bool = False) -> Session:
-    engine = create_engine('postgresql://postgres:postgres@localhost:5432/my_db', echo=echo)
-    session_factory = sessionmaker(bind=engine)
 
+def get_session() -> Session:
     return session_factory()
