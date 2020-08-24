1631adad7ed33b3a21537cdb1e6cf7dd2f5b6f75
andrzej <andrzej.szulc@gmail.com>
2020-08-24 23:30:58 +0200

    nieudane poki co proby ustawienia zmiennych srodowiskowych

--- app/session.py
+++ app/session.py
@@ -1,7 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
+# import os
+#
+# DB_USER = os.getenv('DB_USER')
+# DB_PASSWORD = os.getenv('DB_PASSWORD')
+# DB_NAME = os.getenv('DB_NAME')
 
-engine = create_engine('postgresql://postgres:postgres@localhost:5432/my_db')
+# engine = create_engine(f'postgresql://{DB_USER}:{DB_PASSWORD}@localhost:5432/my_db')
+engine = create_engine(f'postgresql://postgres:postgres@localhost:5432/my_db')
 session_factory = sessionmaker(bind=engine)
 
 
