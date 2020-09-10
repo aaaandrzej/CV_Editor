b7648b4c8772ef8c55d1f5a361de9f26fd64a206
andrzej <andrzej.szulc@gmail.com>
2020-09-10 17:42:36 +0200

    wdrozone prawie wszystkie poprawki po review testow jednostkowych w test_api_cv_post.py, zostal 1 test do uzupelnienia

--- tests/test_api_cv_post.py
+++ tests/test_api_cv_post.py
@@ -1,5 +1,6 @@
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 import pytest
+from sqlalchemy.exc import OperationalError
 
 
 @pytest.fixture
@@ -20,133 +21,180 @@ def client(app):
         yield client
 
 
-@pytest.mark.parametrize('test_input', [{
-    "firstname": "Test",
-    "lastname": "1",
-    "skills": [],
-    "experience": []
-},
+@pytest.mark.parametrize('test_input, replace_skills_with_json_error, replace_experience_with_json_error, expected', [
+(
     {
-        "firstname": "Second Test",
-        "lastname": "User",
-        "skills": [
-            {
-                "skill_name": "skill1",
-                "skill_level": 1
-            },
-            {
-                "skill_name": "skill2",
-                "skill_level": 2
-            }
-        ],
-        "experience": [
-            {
-                "company": "Firma",
-                "project": "Project",
-                "duration": 5
-            }
-        ]
+    'firstname': 'Test',
+    'lastname': '1',
+    'skills': [],
+    'experience': []
     },
+    None,
+    None,
+    ({'success': 'item added'}, 201)
+),
+(
     {
-        "firstname": "3rd Test",
-        "lastname": "User",
-        "dummy_key": "doesn't matter",
-        "skills": [],
-        "experience": [
-            {
-                "company": "Firma",
-                "project": "Project",
-                "duration": 5
-            }
-        ]
+    'firstname': 'Second Test',
+    'lastname': 'User',
+    'skills': [
+        {
+        'skill_name': 'skill1',
+        'skill_level': 1
+        },
+        {
+        'skill_name': 'skill2',
+        'skill_level': 2
+        }],
+    'experience': [
+        {
+        'company': 'Firma',
+        'project': 'Project',
+        'duration': 5
+        }]
     },
+    None,
+    None,
+    ({'success': 'item added'}, 201)
+),
+(
     {
-        "firstname": "3rd Test",
-        "lastname": "User",
-        "dummy_key": "doesn't matter",
-        "skills": [],
-        "experience": [
-            {
-                "company": "Firma",
-                "project": "Project",
-                "duration": 5
-            },
-            {
-                "company": "Firma2",
-                "project": "Project",
-                "duration": 5
-            },
-            {
-                "company": "Firma3",
-                "project": "Project",
-                "duration": 5
-            }
-        ]
-    }
+    'firstname': '3rd Test',
+    'lastname': 'User',
+    'dummy_key': 'doesnt matter',
+    'skills': [],
+    'experience': [
+        {
+        'company': 'Firma',
+        'project': 'Project',
+        'duration': 5
+        }]
+    },
+    None,
+    None,
+    ({'success': 'item added'}, 201)
+),
+(
+    {
+    'firstname': '3rd Test',
+    'lastname': 'User',
+    'dummy_key': 'doesnt matter',
+    'skills': [],
+    'experience': [
+        {
+        'company': 'Firma',
+        'project': 'Project',
+        'duration': 5
+        },
+        {
+        'company': 'Firma2',
+        'project': 'Project',
+        'duration': 5
+        },
+        {
+        'company': 'Firma3',
+        'project': 'Project',
+        'duration': 5
+        }]
+    },
+    None,
+    None,
+    ({'success': 'item added'}, 201)
+),
+(
+    {
+    'firstname': 'no exp',
+    'lastname': 'User',
+    'dummy_key': 'doesnt matter',
+    'skills': []
+    },
+    None,
+    None,
+    ({'success': 'item added'}, 201)
+),
+(
+    {
+    'lastname': 'no firstname',
+    'skills': [],
+    'experience': []
+    },
+    KeyError,
+    KeyError,
+    ({'error': 'bad input data'}, 400)
+),
+(
+    {
+    'firstname': 'no',
+    'lastname': 'no',
+    'skills': [],
+    'experience': []
+    },
+    KeyError,
+    None,
+    ({'error': 'bad input data'}, 400)
+),
+(
+    {
+    'firstname': 'no skills',
+    'lastname': 'User',
+    'experience': [
+        {
+        'company': 'Firma',
+        'project': 'Project',
+        'duration': 5
+        }]
+    },
+    None,
+    KeyError,
+    ({'error': 'bad input data'}, 400)
+),
+# TODO to ponizej nie zadziala:
+# (
+#     {
+#     'firstname': 'no skills',
+#     'lastname': 'User',
+#     'skills': [],
+#     'experience': []
+#     },
+#     None,
+#     OperationalError,
+#     ({'error': 'db error'}, 500)
+# ),
+(
+    {},
+    None,
+    None,
+    ({'error': 'bad input data'}, 400)
+)
 ])
 @patch('app.main.get_session')
 @patch('app.main.replace_skills_with_json')
 @patch('app.main.replace_experience_with_json')
-def test_api_post_cv_with_success(replace_experience_with_json_mock, replace_skills_with_json_mock,
-                                  get_session_mock, test_input, api_cv_post, client):
-    client.post('/api/cv', json=test_input)
+def test_api_post_cv(replace_skills_with_json_mock, replace_experience_with_json_mock, get_session_mock,
+                                test_input, replace_skills_with_json_error, replace_experience_with_json_error,
+                                expected,
+                                api_cv_post, client):
+    client.post('/api/cv', json=test_input)  # TODO docelowo mozna zamienic na json=Mock()
+    if replace_skills_with_json_error is not None:
+        replace_skills_with_json_mock.side_effect = replace_skills_with_json_error
+    if replace_experience_with_json_error is not None:
+        replace_experience_with_json_mock.side_effect = replace_experience_with_json_error
 
     actual = api_cv_post()
-    expected = ({'success': 'item added'}, 201)
+
     assert actual == expected
 
 
-@pytest.mark.parametrize('test_input', [{
-    "lastname": "no firstname",
-    "skills": [],
-    "experience": []
-},
-    {
-        "firstname": "no skills",
-        "lastname": "User",
-        "experience": [
-            {
-                "company": "Firma",
-                "project": "Project",
-                "duration": 5
-            }
-        ]
-    },
-    {
-        "firstname": "no exp",
-        "lastname": "User",
-        "dummy_key": "doesn't matter",
-        "skills": []
-    },
-    {}
-])
-@patch('app.main.get_session')
-@patch('app.main.replace_skills_with_json')
-@patch('app.main.replace_experience_with_json')
-def test_api_post_cv_with_error(replace_experience_with_json_mock, replace_skills_with_json_mock,
-                                get_session_mock, test_input, api_cv_post, client):
-    client.post('/api/cv', json=test_input)
-
-    # TODO tak naprawdę to parametrize tu nie ma znaczenia, bo bez tego mocka poniżej funkcja endpointu daje sukces
-    # Masz rację, gdyż w tym przypadku to, czy będzie ok, czy nie decyduje payload, tylko to, co rzucą funkcje
-    # replace*. Dlatego w tym przypadku parametrize powinien mieć następujące parametry: exception rzucony przez
-    # pierwszą funkcję (możesz podać None, jeśli funkcja ma skończyć się poprawnie i wtedy nie wywołasz w teście
-    # .side_effect), exception rzucony przez drugą funkcję, oczekiwana odpowiedź. I wszystkie kombinacje :)
-    replace_experience_with_json_mock.side_effect = KeyError
-    replace_skills_with_json_mock.side_effect = KeyError
-
-    actual = api_cv_post()
-    expected = ({'error': 'bad input data'}, 400)
-    assert actual == expected
-
-
-def test_api_cv_post_no_db_error(client, api_cv_post, test_input={
-    "firstname": "Test",
-    "lastname": "User",
-    "skills": [],
-    "experience": []
-}):
-    client.post('/api/cv', json=test_input)
-    actual = api_cv_post()
-    expected = ({'error': 'db error'}, 500)
-    assert actual == expected
+# TODO IN PROGRESS
+# @patch('app.session.session')
+# def test_api_cv_post_no_db_error(session_mock, client, api_cv_post, test_input={
+#     'firstname': 'Test',
+#     'lastname': 'User',
+#     'skills': [],
+#     'experience': []
+# }):
+#     actual = client.post('/api/cv', json=test_input)
+#     session_mock.side_effect = OperationalError
+#     # actual = api_cv_post()
+#     expected = ({'error': 'db error'}, 500)
+#     assert actual == expected
